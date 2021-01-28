9fa98d0a32d7bbf7ebfeb16cd386777e605c335a
Malyshev Ivan <868-mia19u804@users.noreply.git.iu7.bmstu.ru>
2021-01-28 20:42:19 +0300

    Refactor .gitignore and delete some trash

--- main.py
+++
@@ -1,8 +0,0 @@
-import random
-import mod1
-
-for i in range(10, 1, -1):
-    print(i, ' fuck\'s left')
-
-print("Hello, world!")
-mod1.print_2("data/data.txt")
--- mod1.py
+++
@@ -1,6 +0,0 @@
-def print_1():
-    print("Fisting ass")
-
-def print_2(filename):
-    with open(filename) as file:
-        print(file.readlines())
