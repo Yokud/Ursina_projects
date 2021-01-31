83739fb44f18d1cc64ae97736166e5fa815ec02d
Malyshev Ivan <868-mia19u804@users.noreply.git.iu7.bmstu.ru>
2021-01-31 03:41:52 +0300

    release menu and add textures to enemies

--- space_vaders/entities.py
+++ space_vaders/entities.py
@@ -1,5 +1,6 @@
 from ursina import *
 
+
 class Spaceship(Sprite):
     def __init__(self, texture=None, shot_speed=0.5, move_speed=1, scale=.125):
         super().__init__(scale=scale, texture=texture, collider='box',
@@ -8,10 +9,25 @@ class Spaceship(Sprite):
         self.shot_speed = shot_speed
         self.move_speed = move_speed
         self.position = Vec2(0, -2 * window.aspect_ratio)
+        self.text_score = Text(text="Score: " + str(self.score), background=True,
+                               x=-0.85, y=-0.45, z=-1)
+        self.floor = Sprite(color=color.green, scale_x=window.fullscreen_size[0] / 100, scale_y=0.2,
+                            position=Vec2(0, -2.3 * window.aspect_ratio), collider='box', collision=True, visible=False)
         
     def update(self):
         self.x += held_keys['d'] * self.move_speed * time.dt if self.x < 6.75 else 0
         self.x -= held_keys['a'] * self.move_speed * time.dt if self.x > -6.75 else 0
+
+        player_hit_info = self.intersects()
+        floor_hit_info = self.floor.intersects()
+
+        if player_hit_info.hit and type(player_hit_info.entity) is Enemy or\
+                floor_hit_info.hit and type(floor_hit_info.entity) is Enemy:
+            destroy(self)
+            scene.clear()
+            menu_bg = Sprite(parent=scene, z=10, scale=(16, 9), texture='data/void.jpg')
+            menu_bg.scale *= 2
+            self.menu()
 
     def input(self, key):
         if key == 'space':
@@ -19,6 +35,16 @@ class Spaceship(Sprite):
 
     def shot(self):
         Bullet(self, position=Vec2(self.x, self.y + 0.25))
+
+    def menu(self):
+        WindowPanel(
+            title='You died',
+            content=(
+                Text("Score: " + str(self.score)),
+                Button(text='Try again', color=color.azure, on_click=HotReloader(path='main.py').reload_code),
+                Button(text='Exit', color=color.red, on_click=application.quit)
+            ), lock_x=True, lock_y=True, lock_z=True
+        )
 
 
 class Bullet(Sprite):
@@ -37,25 +63,17 @@ class Bullet(Sprite):
             destroy(self)
         elif hit_info.hit and type(hit_info.entity) is Enemy:
             destroy(hit_info.entity)
-            destroy(self, 0.1)
+            destroy(self, 0.01)
             self.player.score += 10
+            self.player.text_score.text = "Score: " + str(self.player.score)
         
 
 class Enemy(Sprite):
-    def __init__(self, move_speed=3,
-                 start_position=Vec2(0, 2 * window.aspect_ratio)):
-        super().__init__(color=color.red, scale_x=.5, scale_y=.5,
+    def __init__(self, enemy_texture=None, move_speed=3,
+                 start_position=Vec2(0, 2.5 * window.aspect_ratio)):
+        super().__init__(texture=enemy_texture, scale_x=1, scale_y=1,
                          position=start_position, collider='box', collision=True)
         self.move_speed = move_speed
 
-
     def update(self):
         self.y -= self.move_speed * time.dt
-
-        hit_info = self.intersects()
-
-        if self.y <= -2 * window.aspect_ratio:
-            destroy(self)
-            print("You died")
-            #application.pause()
-            
--- space_vaders/main.py
+++ space_vaders/main.py
@@ -1,11 +1,12 @@
 from entities import Spaceship, Enemy
 from ursina import *
-import random
+import random as rnd
+import os
 
 app = Ursina()
 
 window.title = "Space Vaders"
-#window.icon = "data/modular_ships.png"
+window.icon = "data/SV_ico.ico"
 window.borderless = False
 window.exit_button.visible = False
 window.cursor = False
@@ -15,21 +16,22 @@ bg = Entity(parent=scene, model='quad', z=10, scale=(16, 9),
 bg.scale *= 2
 
 player = Spaceship('data/modular_ships.png', move_speed=4)
+enemy_textures = os.listdir('data/enemies')
 
-invoke(Enemy, move_speed=1,
-       start_position=Vec2(random.randrange(-5, 5), 2 * window.aspect_ratio),
+invoke(Enemy, enemy_texture=enemy_textures[rnd.randrange(len(enemy_textures))], move_speed=1,
+       start_position=Vec2(rnd.randrange(-5, 5), 2 * window.aspect_ratio),
        delay=0.5)
-invoke(Enemy, move_speed=1,
-       start_position=Vec2(random.randrange(-5, 5), 2 * window.aspect_ratio),
+invoke(Enemy, enemy_texture=enemy_textures[rnd.randrange(len(enemy_textures))], move_speed=1,
+       start_position=Vec2(rnd.randrange(-5, 5), 2 * window.aspect_ratio),
        delay=0.5)
-invoke(Enemy, move_speed=1,
-       start_position=Vec2(random.randrange(-5, 5), 2 * window.aspect_ratio),
+invoke(Enemy, enemy_texture=enemy_textures[rnd.randrange(len(enemy_textures))], move_speed=1,
+       start_position=Vec2(rnd.randrange(-5, 5), 2 * window.aspect_ratio),
        delay=0.5)
-invoke(Enemy, move_speed=1,
-       start_position=Vec2(random.randrange(-5, 5), 2 * window.aspect_ratio),
+invoke(Enemy, enemy_texture=enemy_textures[rnd.randrange(len(enemy_textures))], move_speed=1,
+       start_position=Vec2(rnd.randrange(-5, 5), 2 * window.aspect_ratio),
        delay=0.5)
-invoke(Enemy, move_speed=1,
-       start_position=Vec2(random.randrange(-5, 5), 2 * window.aspect_ratio),
+invoke(Enemy, enemy_texture=enemy_textures[rnd.randrange(len(enemy_textures))], move_speed=1,
+       start_position=Vec2(rnd.randrange(-5, 5), 2 * window.aspect_ratio),
        delay=0.5)
 
 app.run()
